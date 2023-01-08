c3f92c85a4acacb02e66337b4f4915ab0f3940ef
malred <99012268+malred@users.noreply.github.com>
2023-01-08 21:25:20 +0800

    Update [id].jsx

--- pages/post/[id].jsx
+++ pages/post/[id].jsx
@@ -33,7 +33,7 @@ export async function getStaticProps({params}) {
                 break;
             default:
                 data = {}
-        }*/ 
+        } 
     return {
         props: {data: res}
     }
